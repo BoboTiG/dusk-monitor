af693f4bcc236e3be5a72d6c3c268c629b672668
Mickaël Schoentgen <contact@tiger-222.fr>
2025-01-07 00:25:32 +0100

    feat: [WIP] persist data

--- README.md
+++ README.md
@@ -1,10 +1,41 @@
 # Dusk Node Monitoring
 
-## Setup
+Simple tool to sync & display Dusk node metrics.
+It is all about blocks, nothing more.
+
+## Install
 
 ```bash
-python -m venv venv
+python3 -m venv venv
 . ./venv/bin/activate
 python -m pip install -U pip
 python -m pip install -r requirements.txt
+```
+
+## Setup
+
+```bash
+cp db.json.sample db.json
+```
+
+Add the provisioner public key in `db.json`.
+
+## Run
+
+Update data on a regular basis (to be done via a daily cron job):
+
+```bash
+python app --update
+```
+
+Start the local web server at [http://localhost:1923](http://localhost:1923):
+
+```bash
+python app
+```
+
+## Test
+
+```bash
+python -m doctest app.py
 ```
--- app.py
+++ app.py
@@ -1,92 +1,135 @@
+"""
+Simple tool to sync & display Dusk node metrics.
+It is all about blocks, nothing more.
+"""
+import json
 import pathlib
 import re
 import subprocess
 
 import flask
 
-PORT = sum(ord(c) for c in "Dusk Node Monitoring")  # 1923
+PROVISIONER = ""
+NODE_URL = "https://"
+HEADERS = {"User-Agent": "https://github.com/BoboTiG/dusk-monitor"}
+DB_FILE = pathlib.Path(__file__).parent / "db.json"
+PORT = sum(ord(c) for c in "Dusk Node Monitoring")  # Hint: one-thousand-twenty-three
 CMD = ["ssh", "-t", "dusk", "source .profile ; blocks"]
+CMD_GENERATED = ["ssh", "-t", "dusk", "source .profile ; list_rejected_blocks"]
+
+Database = dict[str, set[int]]
+
 app = flask.Flask(__name__)
 
 
 @app.route("/")
 def index() -> flask.Response:
+    data = db_load()
+    accepted = len(data["accepted"])
+    generated = len(data["generated"]) + accepted
     output = subprocess.check_output(CMD, text=True)
-    values = [int(value) for value in re.findall(r"m(\d+)", output)]
-    reward = block_reward(values[3])
-    html = f"""\
-<html>
+    current, latest, *_ = [int(value) for value in re.findall(r"m(\d+)", output)]
+    reward = compute_rewards(data["accepted"])
+    ratio = accepted * 100 / (generated or 1)
+    html = f"""<html>
 <head>
     <meta content="width=device-width,initial-scale=1.0" name="viewport">
     <link rel="shortcut icon" href="/static/favicon.svg">
+    <link rel="stylesheet" href="/static/style.css"/>
     <title>Dusk Node Monitoring</title>
-    <style>
-        @font-face {{
-            /* https://github.com/githubnext/monaspace/blob/v1.000/fonts/webfonts/MonaspaceArgonVarVF%5Bwght%2Cwdth%2Cslnt%5D.woff2 */
-            font-family: "Monaspace Argon";
-            src:
-                local("Monaspace Argon Regular"),
-                url("/static/monaspace-argon.woff2") format("woff2");
-        }}
-        body {{
-            height: 100%;
-            padding: 0;
-            margin: 0;
-            background-color: #000;
-            display: flex;
-            flex-flow: column wrap;
-            justify-content: space-around;
-        }}
-        button {{
-            flex: 1;
-            border: 1px solid #111;
-            border-radius: .1em / 1em;
-            text-align: center;
-            font-size: 3em;
-            font-family: "Monaspace Argon";
-            color: #ddd;
-            text-shadow: 1px 1px 0 #222;
-        }}
-        button::after {{
-            display: block;
-            font-size: .3em;
-            color: moccasin;
-        }}
-        #a::after {{ content: "｢Current Block｣" }}
-        #b::after {{ content: "｢Latest Block｣" }}
-        #c::after {{ content: "｢Generated Blocks｣" }}
-        #d::after {{ content: "｢Accepted Blocks｣" }}
-    </style>
 </head>
 <body>
-    <button id="a" style="background:#635985" title="{values[0]:,}">{sizeof_fmt(values[0])}</button>
-    <button id="b" style="background:#443c68" title="{values[1]:,}">{sizeof_fmt(values[1])}</button>
-    <button id="c" style="background:#393053" title="{values[2]:,}">{sizeof_fmt(values[2])}</button>
-    <button id="d" style="background:#18122b" title="{values[3]:,}">
-        <div style="display:block;margin-bottom:-.4em">{sizeof_fmt(values[3])}</div>
-        <span style="font-size:.5em" title="Ratio blocks accepted / blocks generated">{values[3] * 100 / (values[2] or 1):0,.02f}%</span>
-        <span style="font-size:.5em">|</span>
-        <span style="font-size:.5em" title="{reward:0,.02f}">{sizeof_fmt(reward)}$</span>
+    <button id="block-current" title="{current:,}">{format_num(current)}</button>
+    <button id="block-latest" title="{latest:,}">{format_num(latest)}</button>
+    <button id="blocks-generated" title="{generated:,}">{format_num(generated)}</button>
+    <button id="blocks-accepted" title="{accepted:,}">
+        <div>{format_num(accepted)}</div>
+        <span title="Ratio blocks accepted / blocks generated">{ratio:0,.02f}%</span>
+        <span>|</span>
+        <span title="{reward:0,.02f}">{format_num(reward)}$</span>
     </button>
-    <!-- 2025-01-06 -->
+    <!-- First version: 2025-01-06 ! -->
 </body>
 </html>"""
     return flask.Response(html, mimetype="text/html")
 
 
-def block_reward(count: int) -> int:
+def compute_rewards(blocks: set[int]) -> float:
     """
-    As of 2025-01-06:
-        - There is a 19.8574 block reward.
-        - Deducting 10% for the team rewards.
-        - Block generator gets 80% + voting fractions (not computed here).
+    Block generators get 80% + voting fractions (not computed here).
     Source: https://github.com/dusk-network/audits/blob/main/core-audits/2024-09_economic-protocol-design_pol-finance.pdf
     Source: https://github.com/dusk-network/rusk/blob/rusk-1.0.0/rusk/src/lib/node.rs#L132-L157
+    
+    >>> compute_rewards({0})  # Genesis
+    0.0
+    >>> compute_rewards({1 + 1})  # Period 1
+    15.885919999999999
+    >>> compute_rewards({12_614_401 + 1})  # Period 2
+    7.942959999999999
+    >>> compute_rewards({25_228_801 + 1})  # Period 3
+    3.9714799999999997
+    >>> compute_rewards({37_843_201 + 1})  # Period 4
+    1.9857440000000002
+    >>> compute_rewards({50_457_601 + 1})  # Period 5
+    0.9928720000000001
+    >>> compute_rewards({63_072_001 + 1})  # Period 6
+    0.496432
+    >>> compute_rewards({75_686_401 + 1})  # Period 7
+    0.248216
+    >>> compute_rewards({88_300_801 + 1})  # Period 8
+    0.124112
+    >>> compute_rewards({100_915_201 + 1})  # Period 9
+    0.062056
+    >>> compute_rewards({113_529_597})  # Last mint
+    0.043424000000000004
+
+    >>> compute_rewards({10, 10_000_000, 50_000_000, 100_000_000})
+    33.881696
     """
-    return 19.8574 * 0.8 * count
+    amount = 0.0
+    for block in blocks:
+        if block == 113_529_597:  # Last mint
+            dusk = 0.05428
+        elif block >= 100_915_201:  # Period 9
+            dusk = 0.07757
+        elif block >= 88_300_801:  # Period 8
+            dusk = 0.15514
+        elif block >= 75_686_401:  # Period 7
+            dusk = 0.31027
+        elif block >= 63_072_001:  # Period 6
+            dusk = 0.62054
+        elif block >= 50_457_601:  # Period 5
+            dusk = 1.24109
+        elif block >= 37_843_201:  # Period 4
+            dusk = 2.48218
+        elif block >= 25_228_801:  # Period 3
+            dusk = 4.96435
+        elif block >= 12_614_401:  # Period 2
+            dusk = 9.9287
+        elif block >= 1:  # Period 1
+            dusk = 19.8574
+        else:  # Genesis
+            dusk = 0.0
+        amount += dusk * 0.8
+    return amount
 
 
-def sizeof_fmt(value: int) -> str:
+def db_load() -> Database:
+    global PROVISIONER
+
+    data = json.loads(DB_FILE.read_text())
+    PROVISIONER = data["provisioner"]
+    return {
+        "generated": set(data.get("generated", [])),
+        "accepted": set(data.get("accepted", [])),
+    }
+
+
+def db_save(data: Database) -> None:
+    DB_FILE.write_text(json.dumps(data, sort_keys=True))
+
+
+def format_num(value: int) -> str:
     if value < 1000:
         if isinstance(value, float):
             return f"{value:0.02f}"
@@ -100,4 +143,43 @@ def sizeof_fmt(value: int) -> str:
     return f"{value:3,.3f}M"
 
 
-app.run(port=PORT, host="0.0.0.0", debug=True)
+def get_accepted() -> set[int]:
+    import niquests
+
+    # FIXME: Find a more efficient query, like passing `generatorBlsPubkey == "PROVISIONER"` as a condition directly.
+    query = {
+        "topic": "gql",
+        "data": "fragment BlockInfo on Block { header { height, generatorBlsPubkey } } query() { blocks(last: 10000) {...BlockInfo} }",  # noqa: E501
+    }
+    with niquests.post(NODE_URL, headers=HEADERS, json=query) as req:
+        return {
+            block["header"]["height"]
+            for block in req.json()
+            if block["header"]["generatorBlsPubkey"] == PROVISIONER
+        }
+
+
+def get_generated() -> set[int]:
+    output = subprocess.check_output(CMD_GENERATED, text=True)
+    return {int(block) for block in output.strip().split()}
+
+
+def update() -> None:
+    new_generated = get_generated()
+    new_accepted = get_accepted()
+    if not new_generated and not new_accepted:
+        return
+
+    data = db_load()
+    data["generated"] |= new_generated
+    data["accepted"] |= new_accepted
+    db_save(data)
+
+
+if __name__ == "__main__":
+    import sys
+
+    if "--update" in sys.argv:
+        update()
+    else:
+        app.run(port=PORT, host="0.0.0.0", debug=True)
