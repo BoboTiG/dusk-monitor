ad27cbaf89068f1b4ba18ffdaccd0572502981fd
Mickaël Schoentgen <contact@tiger-222.fr>
2025-01-08 23:21:53 +0100

    fix: move import

--- app/__main__.py
+++ app/__main__.py
@@ -3,13 +3,12 @@ This is part of the DnS Dusk node Monitoring.
 Source: https://github.com/BoboTiG/dusk-monitor
 """
 
-import asyncio
-
-
 if __name__ == "__main__":
     import sys
 
     if "--listen" in sys.argv:
+        import asyncio
+
         from app import listen
 
         asyncio.run(listen.to_accepted_blocks())
