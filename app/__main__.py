c05577f5a9abb80748a5cf0562bcfb7b40878554
Mickaël Schoentgen <contact@tiger-222.fr>
2025-01-26 20:25:03 +0100

    feat: implement a simple naive lock file to prevent running a big update twice
    Related to #4.

--- app/__main__.py
+++ app/__main__.py
@@ -3,13 +3,24 @@ This is part of the DnS Dusk node Monitoring.
 Source: https://github.com/BoboTiG/dusk-monitor
 """
 
+from pathlib import Path
+
+
+LOCK_FILE = Path("/tmp/dusk-monitor.lock")
+
+
 if __name__ == "__main__":
     import sys
 
     if "--update" in sys.argv:
-        from app import update
+        if not LOCK_FILE.is_file():
+            LOCK_FILE.write_text("Hello, there!")
+            try:
+                from app import update
 
-        update.update()
+                update.update()
+            finally:
+                LOCK_FILE.unlink()
     else:
         from app import constants, server
 
